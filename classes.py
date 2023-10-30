c1748f352700283a9f13ca4ef8a5ec49df9689a2
Sümeyye Öztürk <s.kaya@msn.com>
2023-10-30 15:45:53 +0300

    write fonksiyonu eklendi

--- classes.py
+++ classes.py
@@ -7,9 +7,12 @@ class Human:
         print(f"{self.name} {self.age} yaşında, {cumle}")
     def walk(self):
         print(f"{self.name} is walking...")
+    def write(self,dokuman):
+        print(f"{self.name},bugün {dokuman} yazıyor")
 
 human1=Human("Ahmed",8)
 human1.name="Bilal"
 human1.talk("nasılsın?")
 human1.name="Önder"
 human1.walk()
+human1.write("makale")
