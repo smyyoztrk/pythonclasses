97a67b05d16244e8e3b2093a48cd4bce37f96651
Sümeyye Öztürk <s.kaya@msn.com>
2023-10-30 15:13:08 +0300

    yapıcı bloğa yaş parametresi eklendi.

--- classes.py
+++ classes.py
@@ -1,13 +1,14 @@
 class Human:
-    def __init__(self,name) -> None:
+    def __init__(self,name,age) -> None:
         self.name=name
+        self.age=age
         print("bir yapıcı blok oluşturuldu")
     def talk(self,cumle):
-        print(f"{self.name}: {cumle}")
+        print(f"{self.name} {self.age} yaşında, {cumle}")
     def walk(self):
         print(f"{self.name} is walking...")
 
-human1=Human("Ahmed")
+human1=Human("Ahmed",8)
 human1.name="Bilal"
 human1.talk("nasılsın?")
 human1.name="Önder"
